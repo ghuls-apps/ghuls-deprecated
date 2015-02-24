111661c6f5ebaabfd0e706806e76efde8fa1396e
satanicsanta <elifosterwy@gmail.com>
2015-02-23 21:57:40 -0800

    Add gitignore. Got started on the replacement for the old code, now using Net::GitHub::V3. Currently getting errors and Im not sure why.

--- src/main.pl
+++ src/main.pl
@@ -1,29 +1,41 @@
 use warnings;
 use diagnostics;
 use strict;
-use WWW::Mechanize;
 use Data::Dumper;
+use Net::GitHub::V3;
 
-print 'Enter a username: ';
-my $user = <>;
-chomp $user;
-my $repos_url = "https://api.github.com/users/$user/repos";
-my $www = WWW::Mechanize->new();
-my $repos = $www->get($repos_url);
-my $repos_decoded = $repos->decoded_content();
-my @repos_list = ($repos_decoded =~ m/\"name\"\:\"(.*?)\"/g);
+my $ERROR = $!;
 
-foreach (@repos_list) {
-    my $this = $_;
-    my $this_url = "https://api.github.com/repos/$user/$this/languages";
-    my $lang = $www->get($this_url);
-    my $lang_decoded = $lang->decoded_content();
-    my @lang_names = ($lang_decoded =~ m/\"(.*?)\":/g);
-    foreach (@lang_names) {
-        my $this_lang = $_;
-        my $lang_num = $lang_decoded =~ m/\"$this_lang\": (.*?)(\,\n\})/;
-        print "$this_lang: $lang_num";
+sub main {
+    print 'Enter a username: ';
+    my $user = <>;
+    chomp $user;
+    my @secure = read_secure();
+    my $git = Net::GitHub::V3->new(
+        access_token => $secure[0]
+    );
+    my $repos = $git->repos;
+    my @repos_list = $repos->list_user($user);
+
+    foreach (@repos_list) {
+        my $this = $_;
+        my @lang_names = $repos->languages->($user, $this);
+        print $this;
+        foreach (@lang_names) {
+            my $this = $_;
+            print $this;
+        }
     }
 }
 
+sub read_secure {
+    my $file = 'secure.txt';
+    open my $fh, '<', $file or die "Could not open '$file' $ERROR\n";
+    my @lines = <$fh>;
+    close $fh;
+    chomp @lines;
+    return @lines;
+}
+
+main();
 exit 0;
