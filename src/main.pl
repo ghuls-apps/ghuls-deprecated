b2b1ba058890ba4ce72a02d49bb0c8fb4389a967
Eli Foster <elifosterwy@gmail.com>
2015-02-26 20:40:29 -0800

    Totals work. Percents do not.

--- src/main.pl
+++ src/main.pl
@@ -14,10 +14,6 @@ sub main {
     my $user = <>;
     chomp $user;
 
-    print "Would you like to include forks in the calculations for $user?\nINCLUDING FORKS MAY MAKE THE CODE TAKE AN IMMENSE AMOUNT OF TIME, AND PROVIDE INACCURATE RESULTS. USE AT YOUR OWN RISK.\n(y/n)\n";
-    my $includeforks = <>;
-    chomp $includeforks;
-
     my @langs = ();
     my @secure = read_secure();
     my $git = Net::GitHub::V3->new(
@@ -28,7 +24,7 @@ sub main {
     my @forks = ();
 
     foreach my $repo (@repos_list) {
-        if ($repo->{fork} == 1 and $includeforks =~ m/^n$/i) {
+        if ($repo->{fork} == 1) {
             push @forks, $repo;
         } else {
             my %lang_names = $repos->languages($user, $repo->{name});
@@ -47,14 +43,17 @@ sub main {
     my @lang_sums = ();
     foreach my $lang (@langs) {
         foreach my $repo (@repos_list) {
-            my %lang_names = $repos->languages($user, $repo->{name});
-            if (exists $lang_names{$lang}) {
-                $sum += $lang_names{$lang};
-                print "$lang: $sum\n";
-                push @lang_sums, $sum;
-                $sum = 0;
+            if ($repo->{fork} == 0) {
+                my %lang_names = $repos->languages($user, $repo->{name});
+                if (exists $lang_names{$lang}) {
+                    print "$lang: $lang_names{$lang}... ";
+                    $sum += $lang_names{$lang};
+                    print "$lang: $sum\n";
+                    push @lang_sums, $sum;
+                }
             }
         }
+        $sum = 0;
     }
 
     my $lang_sums_total = 0;
@@ -63,9 +62,20 @@ sub main {
 
     my @percents = ();
     foreach my $sum (@lang_sums) {
-        my $percent = calc_percentage($sum, $lang_sums_total);
-        print $_ for @langs . ": $percent\n";
-        push @percents, $percent;
+        foreach my $lang (@langs) {
+            foreach my $repo (@repos_list) {
+                if ($repo->{fork} == 0) {
+                    my %lang_names = $repos->languages($user, $repo->{name});
+                    if (exists $lang_names{$lang}) {
+                        my $percent = calc_percentage($sum, $lang_sums_total);
+                        push @percents, $percent;
+                    }
+                    foreach my $percent (@percents) {
+                        print "$lang: $percent ($repo->{name})\n";
+                    }
+                }
+            }
+        }
     }
 }
 
