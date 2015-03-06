c3a863196705c4bf18657e297b7d50820e469006
Eli Foster <elifosterwy@gmail.com>
2015-03-06 09:49:21 -0800

    fs > php

--- src/index.js
+++ src/index.js
@@ -17,32 +17,14 @@ function handleUsername() {
             //"loginPassword":loginPasswordValue,
             "analyzeUsername":input
         }
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "json_handle.php",
-            data: jsonObject,
-            success: function() {
-                alert('Yahooo!');
-            error: function(e) {
-                alert('Error occurred: ' + e);
-            }
-        });
         
-        buffer = new Buffer(input);
-        fs.open(path, 'w', function(err, fd) {
-            if (err) {
-                throw err;
+        var outputFile = 'tmp/data.json';
+        
+        fs.writeFile(outputFile, JSON.stringify(jsonObject, null, 4), function(err) {
+            if(err) {
+                console.log(err);
             } else {
-                fs.write(fd, buffer, 0, buffer.length, null, function(err) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        fs.close(fd, function() {
-                            alert('File written');
-                        })
-                    }
-                });
+                console.log("JSON saved to " + outputFile);
             }
         });
     }
