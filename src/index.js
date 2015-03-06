427bc530b4ab7286219cd74def8e86e029d7ed11
Eli Foster <elifosterwy@gmail.com>
2015-03-06 09:35:20 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 var fs = require('fs');
+var nw = require('nw');
 
 function handleUsername() {
     if (form.username.value == "") {
         alert("You gotta provide input homie");
     } else {
         var input = document.form.username.value
-        var path = 'tmp/user.txt',
+        
+        //var jsonText = '{ "inputInfo": [' + 
+            //'{"loginUsername":"' + loginUsernamevalue + '"},' +
+            //'{"loginPassword":"' + loginPasswordvalue + '"},' +
+            //'"dataUsername":"' + input + '"} ] }';
+        //var jsonObject = JSON.parse(jsonText);
+        var jsonObject = {
+            //"loginUsername":loginUsernameValue,
+            //"loginPassword":loginPasswordValue,
+            "analyzeUsername":input
+        }
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "json_handle.php",
+            data: jsonObject,
+            success: function() {
+                alert('Yahooo!');
+            error: function(e) {
+                alert('Error occurred: ' + e);
+            }
+        });
+        
         buffer = new Buffer(input);
-
         fs.open(path, 'w', function(err, fd) {
             if (err) {
                 throw err;
