7c1a0d1f5ed3ccae01950c01e4e68e2928327b73
Eli Foster <elifosterwy@gmail.com>
2015-04-24 09:37:13 -0700

    Create Gui.pm

---
+++ src/GHULS/Gui.pm
@@ -0,0 +1,9 @@
+package GHULS::Gui;
+use Wx;
+use base 'Wx::App';
+
+sub OnInit {
+    my $self = shift;
+    my $frame = Wx::Frame->new(undef, -1, 'GHULS', [-1, -1], [650, 675]);
+    $frame->show(1);
+}
