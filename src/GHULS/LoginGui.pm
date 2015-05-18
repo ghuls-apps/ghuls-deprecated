c0d8acb8e72fd2d0c6d65478b441268f58ed9dff
Eli Foster <elifosterwy@gmail.com>
2015-05-18 09:25:53 -0700

    Update LoginGui.pm

--- src/GHULS/LoginGui.pm
+++ src/GHULS/LoginGui.pm
@@ -1,16 +1,20 @@
 package GHULS::LoginGui;
 use Wx;
+use warnings;
+use diagnostics;
+use strict;
 use base 'Wx::App';
+use GHULS;
 
 sub OnInit {
     my $self = shift;
-    my $frame = Wx::Frame->new(undef, -1, 'GHULS', [-1, -1], [650, 675]);
-    $frame->Show(1);
+    our $window = Wx::Window->new(undef, -1, 'GHULS', [-1, -1], [650, 675]);
+    $window->Show(1);
     
-    my $text_login = Wx::StaticText->new($frame, -1, 'Login to GitHub', [325, 650], -1);
-    my $usernamectrl = Wx::TextCtrl->new($frame, -1, 'Username', [325, 625], wxDefaultSize, wxTE_SINGLELINE);
-    my $passwordctrl = Wx::TextCtrl->new($frame, -1, 'Password', [325, 600], wxDefaultSize, wxTE_PASSWORD);
-    my $loginbutton = Wx::Button->new($frame, -1, 'Login', [325, 575], [-1, -1]);
+    my $text_login = Wx::StaticText->new($window, -1, 'Login to GitHub', [325, 650], -1);
+    my $usernamectrl = Wx::TextCtrl->new($window, -1, 'Username', [325, 625], wxDefaultSize, wxTE_SINGLELINE);
+    my $passwordctrl = Wx::TextCtrl->new($window, -1, 'Password', [325, 600], wxDefaultSize, wxTE_PASSWORD);
+    my $loginbutton = Wx::Button->new($window, -1, 'Login', [325, 575], [-1, -1]);
     
     Wx::Event->EVT_BUTTON($self, $loginbutton, get_login_data_file($usernamectrl, $passwordctrl);
     return 1;
@@ -23,6 +27,13 @@ sub get_login_data_file {
     
     open my $fh, '>', 'login.txt';
     print $fh "$username\n";
-    print $fh "$password\n";
+    print $fh "$password";
     close $fh;
+    if (GHULS->login == 1) {
+        error();
+    } elsif (GHULS->login == 0) {
+        $window->Close();
+        my $analyzegui = GHULS::AnalyzeUsernameGui->new();
+        $analyzegui->MainLoop();
+    }
 }
