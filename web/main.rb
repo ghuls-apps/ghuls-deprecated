17c1ca59e2f4e95ac25e6b1f495d8d4a6ca8f04c
elifoster <elifosterwy@gmail.com>
2015-10-29 02:02:28 -0700

    Pretty much completed the thing, yo.

--- web/main.rb
+++ web/main.rb
@@ -1,13 +1,32 @@
 require 'sinatra'
+require 'chartkick'
+require 'yaml'
+require 'string-utility'
 require_relative '../utils/utilities'
 
 gh = Utilities.configure_stuff(token: ENV['GHULS_TOKEN'])
+demonyms = YAML.load_file("#{Dir.pwd}/web/public/demonyms.yml")
 
 get '/' do
   erb :index
 end
 
 get '/analyze' do
-  # do something with this data that it gets.
-  # Utilities.analyze(params[:user], gh[:git])
+  adjective = StringUtility.random_line("#{Dir.pwd}/web/public/adjectives.txt")
+  user = params[:user]
+  data = Utilities.analyze(user, gh[:git])
+  colors = []
+  demonym = nil
+  data.each do |l, p|
+    if p == data.values.max
+      if demonyms.key?(l.to_s)
+        demonym = demonyms.fetch(l.to_s)
+      else
+        demonym = "#{l} coder"
+      end
+    end
+    colors.push(Utilities.get_color_for_language(l.to_s, gh[:colors]))
+  end
+  erb :result, locals: { data: data, user: user, colors: colors,
+                         adjective: adjective, demonym: demonym }
 end
