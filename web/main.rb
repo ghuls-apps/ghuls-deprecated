54d18aa7461f6cc577e9422030db39df53bcb83f
ProgramFOX <programfox@hotmail.be>
2015-11-01 15:29:18 +0100

    Avoid globals in web/main.rb

--- web/main.rb
+++ web/main.rb
@@ -4,38 +4,38 @@ require 'yaml'
 require 'string-utility'
 require_relative '../utils/utilities'
 
-$gh = Utilities.configure_stuff(token: ENV['GHULS_TOKEN'])
-$demonyms = YAML.load_file("#{Dir.pwd}/web/public/demonyms.yml")
-$adjective_path = "#{Dir.pwd}/web/public/adjectives.txt"
+gh = Utilities.configure_stuff(token: ENV['GHULS_TOKEN'])
+demonyms = YAML.load_file("#{Dir.pwd}/web/public/demonyms.yml")
+adjective_path = "#{Dir.pwd}/web/public/adjectives.txt"
 
 get '/' do
   erb :index
 end
 
 get '/analyze' do
-  analyze(params[:user])
+  analyze(params[:user], gh, demonyms, adjective_path)
 end
 
 get '/analyze-random' do
-  analyze(Utilities.get_random_username($gh[:git]))
+  analyze(Utilities.get_random_username(gh[:git]), gh, demonyms, adjective_path)
 end
 
-def analyze(user)
-  user_data = Utilities.analyze_user(user, $gh[:git])
-  org_data = Utilities.analyze_orgs(user, $gh[:git])
+def analyze(user, gh, demonyms, adjective_path)
+  user_data = Utilities.analyze_user(user, gh[:git])
+  org_data = Utilities.analyze_orgs(user, gh[:git])
   if user_data != false
-    user_adjective = StringUtility.random_line($adjective_path)
+    user_adjective = StringUtility.random_line(adjective_path)
     user_colors = []
     user_demonym = nil
     user_data.each do |l, p|
       if p == user_data.values.max
-        if $demonyms.key?(l.to_s)
-          user_demonym = $demonyms.fetch(l.to_s)
+        if demonyms.key?(l.to_s)
+          user_demonym = demonyms.fetch(l.to_s)
         else
           user_demonym = "#{l} coder"
         end
       end
-      user_colors.push(Utilities.get_color_for_language(l.to_s, $gh[:colors]))
+      user_colors.push(Utilities.get_color_for_language(l.to_s, gh[:colors]))
     end
 
     variables = {
@@ -47,18 +47,18 @@ def analyze(user)
     }
 
     if org_data != false
-      org_adjective = StringUtility.random_line($adjective_path)
+      org_adjective = StringUtility.random_line(adjective_path)
       org_colors = []
       org_demonym = nil
       org_data.each do |l, p|
         if p == org_data.values.max
-          if $demonyms.key?(l.to_s)
-            org_demonym = $demonyms.fetch(l.to_s)
+          if demonyms.key?(l.to_s)
+            org_demonym = demonyms.fetch(l.to_s)
           else
             org_demonym = "#{l} coder"
           end
         end
-        org_colors.push(Utilities.get_color_for_language(l.to_s, $gh[:colors]))
+        org_colors.push(Utilities.get_color_for_language(l.to_s, gh[:colors]))
       end
 
       variables[:org_data] = org_data
